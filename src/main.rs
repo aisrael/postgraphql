d89b18143dc4de43806b5d41b3bb6642222f8bd0
Alistair Israel <aisrael@gmail.com>
2025-05-31 17:56:08 -0400

    Minimal Axum Web service with health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    // Create our application with a single route
+    let app = Router::new().route("/healthz", get(healthz));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// Handler for the health check endpoint, just returns HTTP 200 "OK"
+async fn healthz() -> &'static str {
+    "OK"
 }
