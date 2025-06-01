3795abac12e9d14a6d4e4ecf6a575f45975589d7
Alistair Israel <aisrael@gmail.com>
2025-05-31 22:26:54 -0400

    Test connects and performs a simple query

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use tokio_postgres::NoTls;
+use tokio_postgres::tls::{NoTls, TlsStream};
+use tokio_postgres::{Client, Connection, Socket};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -11,30 +12,58 @@ pub async fn healthz() -> &'static str {
     "OK"
 }
 
-pub async fn connect() -> Result<(), Error> {
-    let (client, connection) = tokio_postgres::connect(
+pub async fn connect() -> Result<(Client, Connection<Socket, impl TlsStream>), Error> {
+    tokio_postgres::connect(
         "host=localhost user=postgres password=postgres dbname=postgraphql",
         NoTls,
     )
-    .await?;
-
-    Ok(())
+    .await
+    .map_err(|e| Error::PostgresError(e))
 }
 
 #[cfg(test)]
 mod tests {
+    use tokio_postgres::SimpleQueryMessage;
+    use tokio_postgres::SimpleQueryMessage::{CommandComplete, Row, RowDescription};
+
     use super::*;
 
-    #[tokio::test]
-    async fn test_pg() {
-        let result = connect().await;
-        match result {
-            Err(Error::PostgresError(e)) => {
-                eprintln!("{:?}", e);
+    fn debug(sqm: SimpleQueryMessage) {
+        match sqm {
+            Row(row) => {
+                println!("columns: {:?}", row.columns());
+                println!("len: {:?}", row.columns());
+                for i in 0..row.columns().len() {
+                    println!("column {}: {:?}", i, row.columns()[i]);
+                    println!("{}", row.get(i).unwra qp());
+                }
             }
-            _ => {
-                assert!(result.is_ok());
+            CommandComplete(count) => {
+                println!("CommandComplete: {:?}", count);
             }
+            RowDescription(arc) => {
+                println!("RowDescription: {:?}", arc);
+                let columns = arc.as_ref();
+                for column in columns {
+                    println!("Column: {:?}", column);
+                }
+            }
+            _ => panic!("unexpected row kind"),
         }
+    }
+
+    #[tokio::test]
+    async fn test_pg() -> Result<(), Error> {
+        let (client, connection) = connect().await?;
+        tokio::spawn(async move {
+            if let Err(e) = connection.await {
+                eprintln!("connection error: {}", e);
+            }
+        });
+        let rows = client.simple_query("SELECT 1").await?;
+        for row in rows {
+            debug(row)
+        }
+        Ok(())
     }
 }
